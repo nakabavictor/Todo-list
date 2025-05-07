1667c3932df12e013501345678feb10afc1ce028
Victor de Castro Nakabayashi <127894385+nakabavictor@users.noreply.github.com>
2025-05-07 18:14:22 -0300

    evento de click no feito e excluir

--- js/javascript.js
+++ js/javascript.js
@@ -1,12 +1,19 @@
 const todoForm = document.querySelector("#todo-form");
 const todoInput = document.querySelector("#todo-input");
 const todoList = document.querySelector("#todo-list");
+const toolbarr = document.querySelector("#toolbar");
 const editForm = document.querySelector("#edit-form");
 const editInput = document.querySelector("#edit-input");
 const cancelEditBtn = document.querySelector("#cancel-edit-btn");
 const searchInput = document.querySelector("#search-input");
 const eraseBtn = document.querySelector("#erase-button");
 const filterBtn = document.querySelector("#filter-select");
+
+const toggleForms = () => {
+  editForm.classList.toggle("hide");
+  todoForm.classList.toggle("hide");
+  todoList.classList.toggle("hide");
+};
 
 const savetodo = (text) => {
   const todo = document.createElement("div");
@@ -17,7 +24,7 @@ const savetodo = (text) => {
   todo.appendChild(todoTitle);
 
   const icones = document.createElement("div");
-  icones.classList.add("icones");
+  icones.classList.add("todo");
 
   const finishBTN = document.createElement("button");
   finishBTN.classList.add("finish");
@@ -25,12 +32,12 @@ const savetodo = (text) => {
   icones.appendChild(finishBTN);
 
   const editBTN = document.createElement("button");
-  editBTN.classList.add("finish");
+  editBTN.classList.add("edit");
   editBTN.innerHTML = '<i class="fa-solid fa-pen"></i>';
   icones.appendChild(editBTN);
 
   const ExcludeBTN = document.createElement("button");
-  ExcludeBTN.classList.add("finish");
+  ExcludeBTN.classList.add("delete");
   ExcludeBTN.innerHTML = ' <i class="fa-solid fa-x">';
   icones.appendChild(ExcludeBTN);
   todo.appendChild(icones);
@@ -53,4 +60,24 @@ todoForm.addEventListener("submit", (e) => {
   }
 });
 
-document.addEventListener("click", (e) => {});
+document.addEventListener("click", (e) => {
+  const elementoBuscado = e.target;
+  const parente = elementoBuscado.parentNode.parentNode;
+  if (elementoBuscado.classList.contains("finish")) {
+    console.log("clicou pra finalizar");
+    parente.classList.toggle("done");
+  }
+  if (elementoBuscado.classList.contains("edit")) {
+    console.log("clicou pra editar");
+    toggleForms();
+  }
+  if (elementoBuscado.classList.contains("delete")) {
+    console.log("clicou pra excluir");
+    parente.remove();
+  }
+});
+
+cancelEditBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  toggleForms();
+});
